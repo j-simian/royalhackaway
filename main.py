4c32f74342171ef2b342b719cb439ecc87bae08e
Jake Simian <jake.simian@keble.ox.ac.uk>
2023-02-04 11:38:16 +0000

    m

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from renderer import * 
+from game_objects import * 
 from menu import *
 from state import *
 from funs import *
@@ -7,5 +8,6 @@ from funs import *
 state = State()
 renderer = Renderer(state)
 menu = Menu(state)
+initEntities()
 renderer.renderMenu(menu)
 renderer.gameLoop()
