dc864c136fc9a86fdb9d3a0adbc59f81a0f1953b
Rish Naik <rishnaik@Rishs-MacBook-Air.local>
2023-02-04 11:13:14 +0000

    menu state

--- menu.py
+++ menu.py
@@ -7,20 +7,21 @@ pygame.init()
 class Menu():
     def __init__(self, state) -> None:
         self.state = state
-    def set_difficulty(self, value, difficulty):
-    # Do the job here !
-        pass
+    def set_controls(self, player, controls):
+        self.state.controls[player] = controls
+        # 0: wasd, 1: ijkl
 
     def start_the_game(self):
     # Do the job here !
         self.state.screen = 1
         self.menu.disable()
     def render(self, screen):
-        self.menu = pygame_menu.Menu('Welcome', 400, 300,
+        self.menu = pygame_menu.Menu('Welcome', 400, 400,
                        theme=pygame_menu.themes.THEME_BLUE)
 
         self.menu.add.text_input('Name :', default='Catgirl 1')
-        self.menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=self.set_difficulty)
+        self.menu.add.selector('Player 1 Controls:', [["wasd",0], ["ijkl",1]], onchange=(lambda _, y: self.set_controls(0, y)))
+        self.menu.add.selector('Player 2 Controls:', [["wasd",0], ["ijkl",1]], onchange=(lambda _, y: self.set_controls(1, y)))
         self.menu.add.button('Play', self.start_the_game)
         self.menu.add.button('Quit', pygame_menu.events.EXIT)
 
