8a6685107027543e2984996288f210029a89246c
Rish Naik <rishnaik@Rishs-MacBook-Air.local>
2023-02-04 11:37:16 +0000

    added selfs

--- menu.py
+++ menu.py
@@ -24,12 +24,12 @@ class Menu():
             image_path="assets/catbg.jpg",
             drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL
         )
-        self.nyatheme.background_color = menubg
+        self.nyatheme.background_color = self.menubg
         self.nyatheme.title_background_color=(240, 0, 110)
         self.nyatheme.widget_selection_effect=pygame_menu.widgets.RightArrowSelection()
 
         self.menu = pygame_menu.Menu('Welcome', self.state.WIDTH, self.state.HEIGHT,
-                       theme=nyatheme, mouse_motion_selection=True)
+                       theme=self.nyatheme, mouse_motion_selection=True)
 
         self.menu.add.text_input('Name :', default='Catgirl 1')
         self.menu.add.selector('Player 1 Controls:', [["wasd",0], ["ijkl",1]], onchange=(lambda _, y: self.set_controls(0, y)))
