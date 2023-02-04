461f14e64b08a7b1a794d1196b0a78a629763abd
Jake Simian <jake.simian@keble.ox.ac.uk>
2023-02-04 10:57:26 +0000

    menu quit

--- renderer.py
+++ renderer.py
@@ -6,14 +6,16 @@ class Renderer:
         self.width = 800
         self.height = 600
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.flip()
         pygame.display.set_caption("Nya Nya Revolution")
+        pygame.display.flip()
         self.screen.fill((255, 255, 255))
 
     def gameLoop(self):
         running = True
         while running:
             for event in pygame.event.get():
+                self.screen.fill((255, 255, 255))
+                pygame.display.flip()
                 if event.type == pygame.QUIT:
                     running = False
 
