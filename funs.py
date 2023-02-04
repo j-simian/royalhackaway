043e7170ef82396b3c2445620d165a7a3c2f1223
Slush Puppy <46222402+Slush0Puppy@users.noreply.github.com>
2023-02-04 13:42:17 +0000

    functioms

--- funs.py
+++ funs.py
@@ -1,10 +1,13 @@
 from statistics import mean
 
-accuracies=[]
-def onRhythm(timer, offset, bpm,echo=False):
+def findAccuracy(timer, offset, bpm):
     q = (60/bpm)/2  #quarter note length
     accuracy = ((timer-offset) / q) % 1 #0 to 1, 0 or 1 are perfect
     whichNote = round((timer-offset)/(q)) % 2
+    return (accuracy,whichNote)
+
+def onRhythm(timer, offset, bpm,echo=False):
+    (accuracy,whichNote) = findAccuracy(timer,offset,bpm)
     perfectThreshold = 0.25 #tolerance either side of note
     hitThreshold = 0.5
     if echo:
