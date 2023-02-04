f37adb09a2f012e5d8fdb4649952146d6a889d0d
Rish Naik <rishnaik@Rishs-MacBook-Air.local>
2023-02-04 18:00:06 +0000

    refactor

--- game_objects.py
+++ game_objects.py
@@ -18,41 +18,43 @@ def initEntities(state):
     entities["hitb"] = hitbox
     return entities
 
+
+def dashAvailable(accuracy, player, frame):
+    return accuracy == 'perfect' and (player.lastdash+1/2<frame or player.lastdashdir!=player.moving)
+
+
+def handlePress(event, timer, player, control):
+    (accuracy,whichNote)=timer.onRhythm(False)
+    frame = timer.getFullFrame()
+    available = dashAvailable(accuracy, player, frame)
+    if available and event.key in [control['left'], control['right']]:
+        player.lastdash = frame + whichNote/2
+        player.lastdashdir = player.moving
+        player.dash+=MAXVELX*DASHRATIO
+    if event.key == control['left']:
+        player.moving = -1
+    if event.key == control['right']:
+        player.moving = 1
+    if event.key == control['up']:
+        player.jumping = 0.6
+
+def handleRelease(event, player, control):
+    if event.key == control['left'] and player.moving == -1:
+        player.moving = 0
+        if pygame.key.get_pressed()[control['right']]:
+            player.moving = 1
+    if event.key == control['right'] and player.moving == 1:
+        player.moving = 0
+        if pygame.key.get_pressed()[control['left']]:
+            player.moving = -1
+    if event.key == control['down']:
+        player.charging = 0.1
+    if event.key == control['up'] and player.jumping > 0:
+        player.jumping = 0
+
 def handleMove(player, control, event, timer, state):
     if event.type == pygame.KEYDOWN:
-        (accuracy,whichNote)=timer.onRhythm(False)
-        frame = timer.getFullFrame()
-        #print(frame)
-        if event.key == control['left']:
-            player.moving = -1
-            if accuracy == 'perfect' and (player.lastdash+1/2<frame or player.lastdashdir!=player.moving):
-                player.lastdash = frame + whichNote/2
-                player.lastdashdir = player.moving
-                player.dash+=MAXVELX*DASHRATIO
-        if event.key == control['right']:
-            player.moving = 1
-            if accuracy == 'perfect' and (player.lastdash+1/2<frame or player.lastdashdir!=player.moving):
-                player.lastdash = frame + whichNote/2
-                player.lastdashdir = player.moving
-                player.dash+=MAXVELX*DASHRATIO
-        if event.key == control['up']:
-            player.jumping = 0.6
-
-    if event.type == pygame.KEYUP:
-        if event.key == control['left'] and player.moving == -1:
-            if pygame.key.get_pressed()[control['right']]:
-                player.moving = 1
-            else:
-                player.moving = 0
-        if event.key == control['right'] and player.moving == 1:
-            if pygame.key.get_pressed()[control['left']]:
-                player.moving = -1
-            else:
-                player.moving = 0
-        if event.key == control['down']:
-            player.charging = 0.1
-        if event.key == control['up'] and player.jumping > 0:
-            player.jumping = 0
+        handlePress(event, timer, player, control)
 
 class Entity:
     def __init__(self, state):
