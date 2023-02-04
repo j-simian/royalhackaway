24e65515a9939def8f4fbd1a23c9f8cf5d58a673
Slush Puppy <46222402+Slush0Puppy@users.noreply.github.com>
2023-02-04 15:08:45 +0000

    forgot offset

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ state = State()
 clock = pygame.time.Clock()
 renderer = Renderer(state)
 menu = Menu(state)
-timer = Timer(110, 2.20)
+timer = Timer(110, 2.35)
 renderer.renderMenu(menu)
 initEntities(state)
 
