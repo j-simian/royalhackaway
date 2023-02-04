619818a2ddd5d77cb40a2f45fe7dcf2c1f882a58
Tri <aerotriangle@outlook.com>
2023-02-04 14:34:33 +0000

    beat pulse

--- renderer.py
+++ renderer.py
@@ -2,7 +2,7 @@ import pygame
 from game_objects import *
 from funs import *
 
-FLASH = False
+FLASH = True
 
 class Renderer:
     def __init__(self, state):
@@ -12,6 +12,7 @@ class Renderer:
         self.height = self.state.HEIGHT
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.bg = pygame.image.load("./assets/imgs/bg.png").convert()
+        self.bgbeat = pygame.transform.scale(self.bg, (self.width+20, self.height+20))
         pygame.display.set_caption("Nya Nya Revolution")
         pygame.display.flip()
         self.screen.fill((255, 255, 255))
@@ -20,7 +21,7 @@ class Renderer:
         (accuracy,whichNote)=onRhythm(pygame.mixer.music.get_pos()/1000, 0, 110)
 
         if (accuracy=="perfect" and whichNote==0 and FLASH):
-            self.screen.fill((255, 255, 255))
+            self.screen.blit(self.bgbeat, (-10, -10))
         else:
             self.screen.blit(self.bg, (0, 0))
 
@@ -31,4 +32,3 @@ class Renderer:
 
     def renderMenu(self, menu):
         menu.render(self.screen)
-
