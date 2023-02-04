aba8e1b8f442216ba3d3c20eb91a9f19697947dc
Rish Naik <rishnaik@Rishs-MacBook-Air.local>
2023-02-04 11:29:55 +0000

    new menu theme

--- menu.py
+++ menu.py
@@ -17,8 +17,8 @@ class Menu():
             self.state.screen = 1
             self.menu.disable()
     def render(self, screen):
-        self.menu = pygame_menu.Menu('Welcome', 400, 400,
-                       theme=pygame_menu.themes.THEME_BLUE)
+        self.menu = pygame_menu.Menu('Welcome', self.state.WIDTH, self.state.HEIGHT,
+                       theme=pygame_menu.themes.THEME_DARK)
 
         self.menu.add.text_input('Name :', default='Catgirl 1')
         self.menu.add.selector('Player 1 Controls:', [["wasd",0], ["ijkl",1]], onchange=(lambda _, y: self.set_controls(0, y)))
--- state.py
+++ state.py
@@ -1,4 +1,8 @@
 class State():
     def __init__(self):
+        self.WIDTH = 800
+        self.HEIGHT = 600
+
+
         self.screen = 0
         self.controls = [0,1]
