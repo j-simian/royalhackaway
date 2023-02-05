7ad8d9fda7177d2b3e707d0e3af12647f9e11144
Tri <aerotriangle@outlook.com>
2023-02-05 10:43:56 +0000

    new title

--- menu.py
+++ menu.py
@@ -40,7 +40,7 @@ class Menu():
         self.nyatheme.title_font_color=(255,255,255)
         self.nyatheme.widget_font_color=(179,0,83)
         self.nyatheme.widget_font_size=50
-        self.nyatheme.title_font_size=70
+        self.nyatheme.title_font_size=50
         self.nyatheme.title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL
         self.nyatheme.widget_selection_color=(255,92,127)
 
@@ -55,9 +55,9 @@ class Menu():
         self.controlsmenu.add.button('Return', pygame_menu.events.BACK)
 
 
-        self.menu = pygame_menu.Menu('Nya Nya Revolution', self.state.WIDTH, self.state.HEIGHT,
+        self.menu = pygame_menu.Menu('Nyance Nyance Revolution', self.state.WIDTH, self.state.HEIGHT,
                     theme=self.nyatheme, mouse_motion_selection=True)
-        self.menu.add.label("Fight to the beat!")
+        self.menu.add.label("> Beat Beat-up! <")
         self.menu.add.text_input('P1 Name: ', default='Caterina', onchange=(lambda y: self.set_name(0, y)))
         self.menu.add.text_input('P2 Name: ', default='Nyatalie', onchange=(lambda y: self.set_name(1, y)))
 
